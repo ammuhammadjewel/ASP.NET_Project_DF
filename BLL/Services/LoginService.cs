b0b113ea72fc70c6ee8f87b670a31e633e85a030
JEWEL-MIAH <jewelmiah280gmail.com>
2024-01-03 06:04:40 +0600

    Repos Interface EF DTOs

---
+++ BLL/Services/LoginService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class LoginService
+    {
+
+
+
+        public static List<LoginDTO> Get()
+        {
+
+            var data = DataAccessFactory.LoginDataAccess().Get();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Login, LoginDTO>());
+            var mapper = new Mapper(config);
+            var users = mapper.Map<List<LoginDTO>>(data);
+            return users;
+        }
+
+
+
+        public static LoginDTO Get(int id)
+        {
+            var data = DataAccessFactory.LoginDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Login, LoginDTO>());
+            var mapper = new Mapper(config);
+            var user = mapper.Map<LoginDTO>(data);
+            return user;
+        }
+
+
+
+        public static LoginDTO Getusername(string uname)
+        {
+            var data = DataAccessFactory.LoginDataAccess().Get(uname);
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Login, LoginDTO>());
+            var mapper = new Mapper(config);
+            var user = mapper.Map<LoginDTO>(data);
+            return user;
+        }
+
+        public static bool Add(LoginDTO dto)
+        {
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<LoginDTO, Login>());
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Login>(dto);
+            var result = DataAccessFactory.LoginDataAccess().Add(data);
+            return result;
+        }
+
+        public static bool Update(LoginDTO dto)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<LoginDTO, Login>());
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Login>(dto);
+            var result = DataAccessFactory.LoginDataAccess().Update(data);
+            return result;
+
+        }
+        public static bool Delete(int id)
+        {
+
+            var result = DataAccessFactory.LoginDataAccess().Delete(id);
+            return result;
+
+        }
+
+    }
+}
---
+++ DAL/Repos/ApplicationsRepo.cs
@@ -0,0 +1,63 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    class ApplicationsRepo : Repo, IRepo<Apply, int>
+    {
+        public bool Add(Apply obj)
+        {
+            db.Applies.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var jap = Get(id);
+            db.Applies.Remove(jap);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Apply> Get()
+        {
+            return db.Applies.ToList();
+        }
+
+        public Apply Get(int id)
+        {
+            return db.Applies.Find(id);
+        }
+
+        public Apply Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Apply Get(int id, string id2)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*public Apply Get(int id, string id2)
+        {
+           
+        }*/
+
+        public Apply GetbyFK(int id)
+        {
+            return db.Applies.FirstOrDefault(t => t.FK_Nid == id);
+        }
+
+        public bool Update(Apply obj)
+        {
+            var acc = Get(obj.Id);
+            db.Entry(acc).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
